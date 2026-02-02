e75a77a03097fdaa0c099e10472f2d1fa5e5b0ed
Samuel Lau <lausingsamuel@gmail.com>
2026-02-02 15:11:38 -0500

    update requiremetns and finish parser

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,24 @@
 import os
+import numpy as np
+from dataclasses import dataclass
+
+
+# --- Data Classes ---
+@dataclass
+class Source:
+    name: str
+    npz_fwd: str
+    npz_rev: str
+    chrom: str
+    fa_path: str
+    cov_fwd: np.ndarray = None
+    cov_rev: np.ndarray = None
+    seq: str = None
+    start0: int = 0
+    end0: int = 0
+    X_fwd: np.ndarray = None
+    X_rc: np.ndarray = None
+
 def parse_folder(path: str):
     files = os.listdir(path) 
     if (len(files) == 3):
@@ -11,9 +31,9 @@ def parse_folder(path: str):
                 fa = path + f"/{file}"
             else:
                 raise "Parse folder failed! Please use file args"
-    return {"fwd_npz": fwd,
-            "rev_npz": rev,
-            "fa": fa},
+    return {"npz_fwd": fwd,
+            "npz_rev": rev,
+            "fa": fa}
 
 
 
--- process_data.py
+++ process_data.py
@@ -10,8 +10,8 @@ def parse_args():
     parser.add_argument("--name", type=str, default="data1")
     parser.add_argument("--chrom", type=str, required=True)
     parser.add_argument("--folder", type=str)
-    parser.add_argument("--npz-fwd", type=str)
-    parser.add_argument("--npz-rev", type=str)
+    parser.add_argument("--npz_fwd", type=str)
+    parser.add_argument("--npz_rev", type=str)
     parser.add_argument("--fasta", type=str)
     parser.add_argument("--target-windows", type=int, default=10_000)
 
@@ -22,7 +22,21 @@ def main():
     args = parse_args()
     if args.folder:
         res = parse_folder(args.folder)
-        print(res)
+        npz_fwd = res.get("npz_fwd") 
+        npz_rev = res.get("npz_rev")
+        fa_path = res.get("fa")
+    else:
+        npz_fwd = args.npz_fwd
+        npz_rev = args.npz_rev
+        fa_path = args.fasta
+        
+    source = Source(
+    name = args.name,
+    chrom=args.chrom,
+    npz_fwd=npz_fwd,
+    npz_rev=npz_rev,
+    fa_path=fa_path
+    )
 
 if __name__ == "__main__":
     main()
